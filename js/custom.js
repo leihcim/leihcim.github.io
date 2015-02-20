c28185614e5e9b15079c7fa55d07523c5781c213
leihcim <barrybevelaar@hotmail.com>
2015-02-20 13:52:46 +0100

    ease

--- js/custom.js
+++ js/custom.js
@@ -136,7 +136,7 @@ $.extend($.easing,
         $( document ).ready(function() {
       // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
         $('.dropdown').on('show.bs.dropdown', function (e) {
-            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(500,easeInBounce);
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(500,"easeInBounce");
         });
 
         // ADD SLIDEUP ANIMATION TO DROPDOWN //
